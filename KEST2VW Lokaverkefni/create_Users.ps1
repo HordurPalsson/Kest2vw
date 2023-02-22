f0021a43943a975f554b96253264f34a7f232c65
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-22 06:44:36 +0000

    Update

--- KEST2VW Lokaverkefni/create_Users.ps1
+++ KEST2VW Lokaverkefni/create_Users.ps1
@@ -1,8 +1,13 @@
-﻿# Breyta í relative path
-# $filepath = Resolve-Path -Path "\\KEST2VW Lokaverkefninotendur.csv"
-
-
-$csvPath = ".\Documents\GitHub\Kest2vw\KEST2VW Lokaverkefni\notendur.csv"
-
-$notendur = Import-Csv $csvPath 
+﻿$notendur = Import-Csv -Path ".\Documents\GitHub\Kest2vw\KEST2VW Lokaverkefni\notendur.csv"
 $notendur | Format-Table
+
+
+# Virkar ekki
+foreach ($notandi in $notendur) {
+    New-ADUser -Nafn $notandi.nafn -Fornafn $notandi.fornafn -Eftirnafn $notandi.eftirnafn -Hopur $notandi.notendanafn 
+
+    
+}
+    
+
+
