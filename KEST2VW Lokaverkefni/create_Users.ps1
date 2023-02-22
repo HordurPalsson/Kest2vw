305d7a59b9506a3f475b37c7e0607ce5b595b60e
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-22 04:42:21 +0000

    Update

--- KEST2VW Lokaverkefni/create_Users.ps1
+++ KEST2VW Lokaverkefni/create_Users.ps1
@@ -1,3 +1,6 @@
 ﻿# Breyta í relative path
+# $filepath = Resolve-Path -Path "notendur.csv"
+
 $filepath = ".\Documents\Github\Kest2vw\KEST2VW Lokaverkefni\notendur.csv"
+
 Import-Csv $filepath | Format-Table
