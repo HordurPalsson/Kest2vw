8da63d9b8dd63b79af54173db8632cf5d2329e49
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-28 11:17:11 +0000

    Update

--- KEST2VW Lokaverkefni/create_Users.ps1
+++ KEST2VW Lokaverkefni/create_Users.ps1
@@ -5,7 +5,7 @@ $notendur | Format-Table
 # Allir settir í þennan hóp
 $default_Hopur = "Allir"
 # Default password
-$default_Password = Read-Host -AsSecureString
+$default_Password = (Read-Host -AsSecureString "Enter password: ")
 
 foreach ($notandi in $notendur) {
     $nafn = $notandi.nafn
@@ -13,7 +13,6 @@ foreach ($notandi in $notendur) {
     $eftirnafn = $notandi.eftirnafn
     $notendanafn = $notandi.notendanafn
     $hopur = $notandi.hopur
-
 
     #Býr til nýjan notendna út úr upplýsingunum frá csv skránni og setur hann í hóp
     New-LocalUser -Name $notendanafn -FullName $nafn  -Password $default_Password
