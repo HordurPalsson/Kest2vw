bb40963c6bf7e4de0bb91905c30c0585403b263c
HordurPalsson <hordur.palsson9@gmail.com>
2023-02-28 11:12:38 +0000

    Update

--- KEST2VW Lokaverkefni/create_Users.ps1
+++ KEST2VW Lokaverkefni/create_Users.ps1
@@ -1,15 +1,11 @@
-﻿# Þarf RSAT:Active Directory Domain Services and Lightweight...
-# Til þess að kóðinn virki. Finnst í optional features.
-Import-Module ActiveDirectory
-
-# Velur csv skráina
+﻿# Velur csv skráina
 $notendur = Import-Csv -Path ".\KEST2VW Lokaverkefni\notendur.csv"
 $notendur | Format-Table
 
 # Allir settir í þennan hóp
 $default_Hopur = "Allir"
 # Default password
-$default_Password = "pass.123"
+$default_Password = Read-Host -AsSecureString
 
 foreach ($notandi in $notendur) {
     $nafn = $notandi.nafn
@@ -18,10 +14,9 @@ foreach ($notandi in $notendur) {
     $notendanafn = $notandi.notendanafn
     $hopur = $notandi.hopur
 
-    $fulltnafn = $fornafn + " " + $eftirnafn    
 
     #Býr til nýjan notendna út úr upplýsingunum frá csv skránni og setur hann í hóp
-    New-LocalUser -Name $notendanafn -FullName $fulltnafn  -Password (Read-Host -AsSecureString $default_Password) 
+    New-LocalUser -Name $notendanafn -FullName $nafn  -Password $default_Password
     Add-LocalGroupMember -Group $default_Hopur -Member $notendanafn
     Add-LocalGroupMember -Group $hopur -Member $notendanafn
 }
